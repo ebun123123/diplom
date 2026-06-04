ccb33a65329f1d0c024590137444eb7abc199072
ilyos <gggg84503@gmail.com>
2026-06-05 09:22:23 +1000

    Ignore all warnings and eslint rules during build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { CountryProvider } from './context/CountryContext'; 
 import Navbar from './components/Navbar';
