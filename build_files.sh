244765e7d48fccc8fceda174858bf96a282aa275
ilyos <gggg84503@gmail.com>
2026-06-25 12:31:12 +1000

    fix: move migrations to build phase via package.json scripts

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-echo "BUILD START"
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py migrate --noinput
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+#!/bin/bash
+echo "=== СБОРКА БЭКЕНДА СТАРТОВАЛА ==="
+python3.9 -m pip install -r backend/requirements.txt
+python3.9 backend/manage.py collectstatic --noinput --clear
+python3.9 backend/manage.py migrate --noinput
+echo "=== СБОРКА БЭКЕНДА ЗАВЕРШЕНА ==="
