df072293871b19b0e95aad55aced9b71238c8f11
ilyos <gggg84503@gmail.com>
2026-06-26 01:12:55 +1000

    2222222222e dsad

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ children }) => {
+export default function ProtectedRoute({ children }) {
   const isAuthenticated = !!localStorage.getItem('diplom_user'); 
 
   if (!isAuthenticated) {
@@ -9,6 +9,4 @@ const ProtectedRoute = ({ children }) => {
   }
 
   return children;
-};
-
-export default ProtectedRoute;
+}
